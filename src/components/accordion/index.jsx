838607ab7a3082dee3fa7a7b2e90a8af692f7ab1
chris <chrisj21283@gmail.com>
2024-03-04 08:26:24 -0500

    display questions

--- src/components/accordion/index.jsx
+++ src/components/accordion/index.jsx
@@ -4,8 +4,17 @@ export default function Accordion() {
 
     console.log(data)
     return (
-        <div>
-            <h1>Hello World</h1>
+        <div className="wrapper">
+            <div className="accordian">
+                {data && data.length > 0 ?
+                    data.map((dataItem) => (
+                        <div className="item">
+                            <h3>{dataItem.question}</h3>
+                            <div className="content">{dataItem.answer}</div>
+                        </div>))
+                    : <div>No data found</div>
+                }
+            </div>
         </div>
     )
 }
