857ff390907a4e01960079dc42843c2f330e3b09
chris <chrisj21283@gmail.com>
2024-03-04 08:29:38 -0500

    add clickable span to show answers

--- src/components/accordion/index.jsx
+++ src/components/accordion/index.jsx
@@ -1,20 +1,33 @@
 import data from "./data"
-
+import { useState } from 'react'
 export default function Accordion() {
 
-    console.log(data)
-    return (
-        <div className="wrapper">
-            <div className="accordian">
-                {data && data.length > 0 ?
-                    data.map((dataItem) => (
-                        <div className="item">
+    const [selected, setSelected] = useState(null)
+
+    function clickHandle(currentId) {
+        setSelected(currentId)
+
+
+    }
+
+    return <div className="wrapper">
+        <div className="accordian">
+            {data && data.length > 0 ?
+                data.map((dataItem) => (
+                    <div className="item">
+                        <div onClick={() => clickHandle(dataItem.id)} className="title">
                             <h3>{dataItem.question}</h3>
-                            <div className="content">{dataItem.answer}</div>
-                        </div>))
-                    : <div>No data found</div>
-                }
-            </div>
+                            <span>+</span>
+                            {
+                                selected === dataItem.id ?
+                                    <div className="content">{dataItem.answer}</div>
+                                    : null
+
+                            }
+                        </div>
+                    </div>))
+                : <div>No data found</div>
+            }
         </div>
-    )
+    </div>
 }
