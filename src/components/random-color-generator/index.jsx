6d3233e3b430ccc604b4c75309c9c9031160b330
Chrispy <chrisj21283@gmail.com>
2024-03-04 20:56:59 -0500

    add hex randomizer

--- src/components/random-color-generator/index.jsx
+++ src/components/random-color-generator/index.jsx
@@ -3,15 +3,41 @@ import './styles.css'
 
 
 export default function Random() {
-const {colorType, setColorType}= useState('hex')
+const [colorCodeType, setColorCode]= useState('')
 const [color, setColor] = useState('')
+
+function randomizer(length){
+    return Math.floor(Math.random()*length)
+}
+
+function handleHexColor() {
+    const hex = [1, 2, 3, 4, 5, 6, 7, 8, 9,"A","B","C","D","E","F"]
+    let hexColor = "#";
+
+    for(let i=0;i<6; i++){
+        hexColor += hex[randomizer(hex.length)]
+    }
+    console.log(hexColor)
+    setColor(hexColor)
+}
+function handleRgbColor() {
+
+}
+
+
 
 return (
 
-<div className="container">
-    <button>Generate Random Color</button>
-    <button>Create Hex Color</button>
-    <button>Create RGB Color</button>
+<div className="container" style={{background:color}}>
+    <button onClick={()=> setColorCode('hex')}>Create Hex Color</button>
+    <button onClick={()=> setColorCode('rgb')}>Create RGB Color</button>
+    <button onClick={colorCodeType === 'hex' ? handleHexColor : handleRgbColor}>Generate Random Color</button>
+    <div>
+
+    </div>
+    <div>
+
+    </div>
 </div>
 )
 }
